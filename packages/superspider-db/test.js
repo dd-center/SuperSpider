c8bd88ed0e12372f1ed93f0aa8e8dd2364d843e6
Il Harper <afanyiyu@hotmail.com>
2019-09-26 21:46:43 +0800

    test: 💍 DB: Test file changes

--- packages/superspider-db/test.js
+++ packages/superspider-db/test.js
@@ -1,25 +1,4 @@
-// const rp = require('request-promise-native')
-
-// ;(async () => {
-//   const data = JSON.parse(
-//     await rp({
-//       uri: 'https://api.live.bilibili.com/av/v1/SuperChat/messageInfo',
-//       qs: {
-//         id: 17
-//       }
-//     })
-//   )
-//   console.log(
-//     data.data.id +
-//       ':' +
-//       data.data.uid +
-//       ':' +
-//       data.data.price +
-//       ':' +
-//       data.data.message +
-//       ':' +
-//       data.data.message_jpn
-//   )
-// })()
-
-console.log(new Date().getTime())
+const adb = require('./modules/adb')
+;(async () => {
+  await adb()
+})()
