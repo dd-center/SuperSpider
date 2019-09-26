778fa67b60f9d386335ee3267ee04dea84393f7d
Il Harper <afanyiyu@hotmail.com>
2019-09-26 22:19:14 +0800

    feat: 🎸 DB: Add Koa Modules

--- packages/superspider-db/index.js
+++ packages/superspider-db/index.js
@@ -1,3 +1,8 @@
+const Koa = require('koa')
+const app = new Koa()
+const Router = require('koa-router')
+const router = new Router()
+
 const schedule = require('node-schedule')
 
 const adb = require('./modules/adb')
@@ -15,4 +20,24 @@ schedule.scheduleJob('*/5 * * * *', adb)
 ;(async () => {
   await adb()
   await rdb()
+
+  app.use(bodyParser())
+
+  // 其他页面通过 router 加载
+  let api = fs.readdirSync(__dirname + '/api')
+  api.forEach((element) => {
+    let module = require(__dirname + '/api/' + element)
+    /*
+      api 下面的每个文件负责一个特定的功能，分开管理
+      通过 fs.readdirSync 读取 api 目录下的所有文件名，挂载到 router 上面
+    */
+    router.use(
+      '/' + element.replace('.js', ''),
+      module.routes(),
+      module.allowedMethods()
+    )
+  })
+  app.use(router.routes())
+
+  app.listen(2161)
 })()
