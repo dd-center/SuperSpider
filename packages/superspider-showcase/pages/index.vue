a44e37cf35ce2a74f963f47bc7a9111f3ee9f558
Il Harper <afanyiyu@hotmail.com>
2019-09-29 17:02:53 +0800

    test: 💍 服务器被炸了，正在伤心中。

--- packages/superspider-showcase/pages/index.vue
+++ packages/superspider-showcase/pages/index.vue
@@ -1,20 +1,24 @@
 <template>
-  <h1>Hi!</h1>
+  <div>
+    <p>服务器炸了，我很伤心，现在准备构建分布式的方案</p>
+    <p>这两天的SC数据并不会漏记，大家请放心</p>
+    <p>吃有服务器的人，吃冰 箱 人（中译日），有什么问题都可以加QQ1302744182</p>
+  </div>
 </template>
 
 <script>
-export default {
-  mounted() {
-    if (this.$route.query.roomid)
-      this.$router.push({
-        path: '/utils/sc',
-        query: {
-          roomid: this.$route.query.roomid
-        }
-      })
-    else this.$router.push('/utils/sc')
-  }
-}
+// export default {
+//   mounted() {
+//     if (this.$route.query.roomid)
+//       this.$router.push({
+//         path: '/utils/sc',
+//         query: {
+//           roomid: this.$route.query.roomid
+//         }
+//       })
+//     else this.$router.push('/utils/sc')
+//   }
+// }
 </script>
 
 <style></style>
