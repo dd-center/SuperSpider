5b1103f291377287fabb07d991d40628aa6abe52
Il Harper <afanyiyu@hotmail.com>
2019-09-29 18:03:11 +0800

    test: 💍 又改回来了

--- packages/superspider-showcase/pages/index.vue
+++ packages/superspider-showcase/pages/index.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
-    <p>服务器炸了，我很伤心，现在准备构建分布式的方案</p>
-    <p>这两天的SC数据并不会漏记，大家请放心</p>
-    <p>吃有服务器的人，吃冰 箱 人（中译日），有什么问题都可以加QQ1302744182</p>
+    <h1>Hi!</h1>
   </div>
 </template>
 
 <script>
-// export default {
-//   mounted() {
-//     if (this.$route.query.roomid)
-//       this.$router.push({
-//         path: '/utils/sc',
-//         query: {
-//           roomid: this.$route.query.roomid
-//         }
-//       })
-//     else this.$router.push('/utils/sc')
-//   }
-// }
+export default {
+  mounted() {
+    if (this.$route.query.roomid)
+      this.$router.push({
+        path: '/utils/sc',
+        query: {
+          roomid: this.$route.query.roomid
+        }
+      })
+    else this.$router.push('/utils/sc')
+  }
+}
 </script>
 
 <style></style>
