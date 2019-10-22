21d1b411939c7ceb519fb602ae27e9103620ccda
Il Harper <afanyiyu@hotmail.com>
2019-10-22 23:14:42 +0800

    fix: 🐛 Remove Koa Router

--- packages/superspider-mdb/index.js
+++ packages/superspider-mdb/index.js
@@ -32,21 +32,21 @@ process.on('uncaughtException', (err) => {
 
   app.use(bodyParser())
 
-  // 其他页面通过 router 加载
-  let api = fs.readdirSync(__dirname + '/api')
-  api.forEach((element) => {
-    let module = require(__dirname + '/api/' + element)
-    /*
-      api 下面的每个文件负责一个特定的功能，分开管理
-      通过 fs.readdirSync 读取 api 目录下的所有文件名，挂载到 router 上面
-    */
-    router.use(
-      '/' + element.replace('.js', ''),
-      module.routes(),
-      module.allowedMethods()
-    )
-  })
-  app.use(router.routes())
+  // // 其他页面通过 router 加载
+  // let api = fs.readdirSync(__dirname + '/api')
+  // api.forEach((element) => {
+  //   let module = require(__dirname + '/api/' + element)
+  //   /*
+  //     api 下面的每个文件负责一个特定的功能，分开管理
+  //     通过 fs.readdirSync 读取 api 目录下的所有文件名，挂载到 router 上面
+  //   */
+  //   router.use(
+  //     '/' + element.replace('.js', ''),
+  //     module.routes(),
+  //     module.allowedMethods()
+  //   )
+  // })
+  // app.use(router.routes())
 
   app.listen(2166)
 
