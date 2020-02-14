c3e3cee86542aa1438691fde71d169603b033716
Il Harper <afanyiyu@hotmail.com>
2020-02-14 16:36:54 +0800

    feat: 🎸 Showcase: Layout Changes

--- packages/superspider-showcase/pages/sc/obs.vue
+++ packages/superspider-showcase/pages/sc/obs.vue
@@ -1,132 +1,5 @@
 <template>
   <el-container :style="`height: 100vh; ${fontStyle}`">
-    <section
-      class="sider-label"
-      :style="`background-color: ${bgColor}; height: 100vh; width: 450px;`"
-    >
-      <sider-scrollbar tag="div" style="height: 100vh;" :block-style="true">
-        <el-row class="sider-scrollbar-item">
-          <div align-center style="text-align: center;">
-            <img
-              src="~/assets/Logo_Trans.png"
-              height="150px"
-              width="150px"
-              style="text-align: center;"
-            />
-            <p style="margin: 0;">v1.0.6</p>
-          </div>
-        </el-row>
-        <el-row class="sider-scrollbar-item">
-          <el-button-group class="sider-scrollbar-item">
-            <el-button
-              size="small"
-              type="primary"
-              plain
-              :style="fontStyle"
-              @click="openLink('https://docs.bilisc.com')"
-              >{{ $t('common.tutorial') }}</el-button
-            >
-            <el-button
-              size="small"
-              type="primary"
-              plain
-              :style="fontStyle"
-              @click="openLink('http://chat.bilisc.com')"
-              >{{ $t('common.chat') }}</el-button
-            >
-          </el-button-group>
-        </el-row>
-        <el-row class="sider-scrollbar-item">
-          <el-form
-            ref="form"
-            label-width="170px"
-            size="mini"
-            label-position="left"
-            @submit.native.prevent
-          >
-            <el-form-item :label="$t('common.lang')">
-              <el-select v-model="$i18n.locale">
-                <el-option label="日本語" value="ja"></el-option>
-                <el-option label="中文" value="zh"></el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item :label="$t('common.showTime')">
-              <el-switch v-model="showTimeNative"></el-switch>
-            </el-form-item>
-            <el-form-item :label="$t('common.showGift')">
-              <el-switch v-model="showGiftNative"></el-switch>
-            </el-form-item>
-            <el-form-item :label="$t('common.giftFilter')">
-              <el-switch v-model="giftFilterNative"></el-switch>
-            </el-form-item>
-            <el-form-item :label="$t('common.showKana')">
-              <el-switch
-                v-if="$i18n.locale == 'ja'"
-                v-model="showKanaNative"
-              ></el-switch>
-              <el-switch
-                v-if="$i18n.locale !== 'ja'"
-                :value="false"
-                disabled
-              ></el-switch>
-            </el-form-item>
-            <el-form-item :label="$t('common.channelid')">
-              <el-input
-                v-model="room"
-                @keyup.enter.native="startFetchData"
-              ></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button
-                type="primary"
-                plain
-                icon="el-icon-s-promotion"
-                @click="startFetchData"
-                @keyup.enter.native="startFetchData"
-                >Go</el-button
-              >
-            </el-form-item>
-          </el-form>
-        </el-row>
-        <el-row class="sider-scrollbar-item">
-          <el-button-group class="sider-scrollbar-item">
-            <el-button
-              size="small"
-              type="primary"
-              plain
-              :style="fontStyle"
-              @click="openLink(addText, true)"
-              >{{ $t('common.standalone') }}</el-button
-            >
-            <el-button
-              size="small"
-              type="primary"
-              plain
-              :style="fontStyle"
-              @click="copyText"
-              >{{ $t('common.copy') }}</el-button
-            >
-          </el-button-group>
-        </el-row>
-        <el-row class="sider-scrollbar-item">
-          <p>{{ $t('sc.t1') }}</p>
-          <p>{{ $t('sc.t2') }}</p>
-          <p>{{ $t('sc.t3') }}</p>
-          <p>
-            {{ $t('sc.t4')
-            }}<a href="https://faithtown.tech/" target="_blank">Il Harper</a
-            >{{ $t('sc.t5') }}
-          </p>
-          <p>{{ $t('sc.t6') }}</p>
-
-          <a target="_blank" href="https://github.com/dd-center/SuperSpider"
-            ><img
-              alt="Star BiliSC! "
-              src="https://img.shields.io/github/stars/dd-center/superspider?color=brightgreen&label=github%20stars&style=flat-square"
-          /></a>
-        </el-row>
-      </sider-scrollbar>
-    </section>
     <sider-scrollbar tag="section" style="width: 100%;">
       <el-container align="center" style="width: 100%; overflow-y: overlay;">
         <el-container align="center" style="display: block;">
@@ -145,16 +18,6 @@
               :key="item._id"
               style="margin: 20px;"
             >
-              <!--
-                              :title="
-                  item.uname +
-                    ($i18n.locale !== 'ja'
-                      ? ''
-                      : item.unamejpn
-                      ? ' (' + item.unamejpn + ')'
-                      : '')
-                "
-            -->
               <Superchat
                 v-if="
                   Number(item.hide) == 0 &&
@@ -200,6 +63,7 @@ export default {
   head: {
     title: 'BiliSC'
   },
+  loading: false,
   layout: 'empty',
   components: {
     Superchat,
@@ -291,7 +155,7 @@ export default {
   methods: {
     fetchAdd() {
       this.addText =
-        'http://127.0.0.1:2164/sc/obs?roomid=' +
+        'https://bilisc.com/sc/obs?roomid=' +
         (this.room || this.$t('common.channelid')) +
         '&showTime=' +
         this.showTimeNative +
@@ -346,12 +210,13 @@ export default {
           'roomid=' + this.room + (this.giftFilterNative ? '&filter=on' : ''),
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
-        }
+        },
+        progress: false
       }).catch((e) => {
         err = true
       })
       if (err) return
-      this.$nuxt.$loading.start()
+      // this.$nuxt.$loading.start()
       // this.scData = scData.data
       for (const item of scData.data) {
         item.data.sort((a, b) => Number(b.ts) - Number(a.ts))
@@ -364,57 +229,8 @@ export default {
         if (his === 2) this.scData.push({ ...item, history: true })
         else this.scData.push(item)
       }
-      this.$nuxt.$loading.finish()
-    },
-    openLink(link, extra) {
-      if (extra)
-        window.open(
-          link,
-          'BiliSC for OBS',
-          'menubar=0,location=0,scrollbars=0,toolbar=0,width=600,height=600'
-        )
-      else window.open(link)
+      // this.$nuxt.$loading.finish()
     }
   }
 }
 </script>
-
-<style>
-.el-menu-item,
-.el-submenu__title {
-  height: auto;
-}
-
-.sider-scrollbar-item {
-  margin: 10px 24px;
-}
-
-.el-form-item__label,
-.el-switch__label,
-.el-link,
-.el-link--default,
-.el-link.el-link--default,
-.sider-label {
-  color: white;
-  /* font-size: 14px; */
-}
-
-.el-button--primary.is-plain {
-  color: white;
-  background: none;
-  border-color: white;
-}
-
-.el-input__inner {
-  background: none;
-  color: white;
-}
-
-.el-form-item__content > .el-switch {
-  float: right;
-}
-
-.el-button-group {
-  float: initial;
-}
-</style>
