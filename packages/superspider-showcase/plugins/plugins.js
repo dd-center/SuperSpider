f6c70ac2d7df95b900523cce6369044a9f97a484
Il Harper <afanyiyu@hotmail.com>
2020-02-27 01:17:45 +0800

    feat: 🎸 Showcase: Add Sentry Module

--- packages/superspider-showcase/plugins/plugins.js
+++ packages/superspider-showcase/plugins/plugins.js
@@ -1,4 +1,11 @@
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
 import Vue from 'vue'
 import VueClipboard from 'vue-clipboard2'
 
 Vue.use(VueClipboard)
+
+Sentry.init({
+  dsn: 'https://18a7709b012b4e6ea60c91a1853f3b5a@sentry.io/2987713',
+  integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+})
